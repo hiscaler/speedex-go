e2043a105ca75b64800dc44cb1923cbd5a680fef
hiscaler <hiscaler@gmail.com>
2025-09-30 09:50:24 +0800

    ...

--- order.service.go
+++ order.service.go
@@ -212,20 +212,18 @@ func (s orderService) Cancel(ctx context.Context, req CancelOrderRequest) ([]Ord
 type OrderEstimateRequest = CreateOrderRequest
 
 // Estimate 订单费用试算
-func (s orderService) Estimate(ctx context.Context, req OrderEstimateRequest) (*entity.OrderEstimateResult, error) {
-	if err := req.Validate(); err != nil {
-		err = invalidInput(err)
-		return nil, err
+func (s orderService) Estimate(ctx context.Context, req OrderEstimateRequest) (res entity.OrderEstimateResult, err error) {
+	if err = req.Validate(); err != nil {
+		return res, invalidInput(err)
 	}
 
-	var res entity.OrderEstimateResult
 	resp, err := s.httpClient.R().
 		SetContext(ctx).
 		SetBody(req).
 		SetResult(&res).
 		Post("/external/orders/estimate")
 	if err = recheckError(resp, err); err != nil {
-		return nil, err
+		return res, err
 	}
-	return &res, nil
+	return res, nil
 }
