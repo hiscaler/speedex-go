2a6b047435869d7255e46ea6b393d96f7f1dc798
hiscaler <hiscaler@gmail.com>
2025-10-10 17:52:51 +0800

    ...

--- entity/order.estimate.result.go
+++ entity/order.estimate.result.go
@@ -1,5 +1,7 @@
 package entity
 
+import "gopkg.in/guregu/null.v4"
+
 // OrderEstimateResult 订单费用试算
 type OrderEstimateResult struct {
 	CustomerOrderNo string                      `json:"customerOrderNo"` // 客户单号
@@ -8,12 +10,12 @@ type OrderEstimateResult struct {
 
 // OrderEstimateProductPrice 订单费用试算-商品费用
 type OrderEstimateProductPrice struct {
-	ProductCode         string `json:"productCode"`
-	ProductId           int    `json:"productId"`
-	ProductName         string `json:"productName"`
-	Currency            string `json:"currency"`
-	Price               string `json:"price"`
-	SellingFreightPrice string `json:"sellingFreightPrice"`
-	SellingMiscFeePrice string `json:"sellingMiscFeePrice"`
-	FailReason          string `json:"failReason"`
+	ProductCode         string      `json:"productCode"`
+	ProductId           int         `json:"productId"`
+	ProductName         string      `json:"productName"`
+	Currency            string      `json:"currency"`
+	Price               string      `json:"price"`
+	SellingFreightPrice string      `json:"sellingFreightPrice"`
+	SellingMiscFeePrice string      `json:"sellingMiscFeePrice"`
+	FailReason          null.String `json:"failReason"`
 }
